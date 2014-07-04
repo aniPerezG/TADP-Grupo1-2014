9108eff98caae07416cc6494e88fdce55e58fd9b
fer <fer_dt1@hotmail.com>
2014-07-04 14:49:24 -0300

    un test de prueba

--- comoViajo/test/comoViajoTest/pruebaTest.scala
+++ comoViajo/test/comoViajoTest/pruebaTest.scala
@@ -1,8 +1,8 @@
 package comoViajoTest
 
 import org.junit.Test
-import org.junit.Assert
-import org.scalatest.FlatSpec
+import org.junit.Assert._
+import org.scalatest._
 import org.scalamock.MockFactoryBase
 import org.scalamock.scalatest
 import comoViajo._
@@ -13,14 +13,28 @@ import org.scalamock.MockFunction
 
 
 
-class pruebaTest extends FlatSpec with MockitoSugar  {
+class pruebaTestCase extends FlatSpec with MockitoSugar  {
 
  
 	@Test
-		def testUsandoRecorridoParaVerSiFuncionaBien()  =  {
-
-			val recorridoPrueba = mock(classOf[Recorrido])
+		def testCreamosUnRecorridoYLePedimosSuCosto  =  {
+			val direc1 = new Direccion("Onsari", 600)
+			val direc2 = new Direccion("Onsari",2500 )
+			val direc3 = new Direccion("Onsari" ,3500)
 			
+			var paradas : Array[Direccion] = new Array[Direccion](3)
+			
+			paradas :+direc1
+			paradas :+direc2
+			paradas :+direc3
+			
+			val informador = mock(classOf[InformacionTransportes])
+			var linea17 = new Colectivo(informador,paradas)
+			val meSubo = new Direccion("onsari",800)
+			val meBajo = new Direccion("onsari", 3500)
+			var miRecorrido = new Recorrido (meSubo,meBajo,linea17)
+			
+			assert(miRecorrido.costoBase == 2.5)
 			
 	}
 }
