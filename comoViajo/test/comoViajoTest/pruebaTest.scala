ed7e452da5633b3e9bf3109438e47dcbbe78942c
fer <fer_dt1@hotmail.com>
2014-07-04 13:38:32 -0300

    carpeta para test
    creado el source y el package para meter los test

---
+++ comoViajo/test/comoViajoTest/pruebaTest.scala
@@ -0,0 +1,26 @@
+package comoViajoTest
+
+import org.junit.Test
+import org.junit.Assert
+import org.scalatest.FlatSpec
+import org.scalamock.MockFactoryBase
+import org.scalamock.scalatest
+import comoViajo._
+import org.scalamock.MockFunction
+import org.scalamock._
+import org.scalatest.mock.MockitoSugar
+import org.scalamock.MockFunction
+
+
+
+class pruebaTest extends FlatSpec with MockitoSugar  {
+
+ 
+	@Test
+		def testUsandoRecorridoParaVerSiFuncionaBien()  =  {
+
+			val recorridoPrueba = mock(classOf[Recorrido])
+			
+			
+	}
+}
