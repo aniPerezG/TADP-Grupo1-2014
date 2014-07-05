46b418a9a3ff92fb6a5e0fac4ade29f2ba713589
Ani <anitaperez2@gmail.com>
2014-07-05 10:30:29 -0300

    agrego los tests de los costos de los viajes compuestos

--- comoViajo/tests/funcionalObjetos/TestsCostos.scala
+++ comoViajo/tests/funcionalObjetos/TestsCostos.scala
@@ -15,6 +15,7 @@ import comoViajo.TarjetaDiscapacitado
 import comoViajo.TarjetaTrabajador
 import comoViajo.ViajeSimple
 import comoViajo.TarjetaTurista
+import comoViajo.ViajeCompuesto
 
 class TestsCostos {
 
@@ -23,11 +24,17 @@ class TestsCostos {
     val paradaColec2 = new Direccion("Onsari", 2500)
     val paradaColec3 = new Direccion("Onsari", 3500)
 
-    val paradaSubte1 = new Direccion("Corrientes", 5000)
-    val paradaSubte2 = new Direccion("Corrientes", 4500)
-    val paradaSubte3 = new Direccion("Corrientes", 4000)
-    val paradaSubte4 = new Direccion("Corrientes", 3500)
-    val paradaSubte5 = new Direccion("Corrientes", 3000)
+    val paradaSubteB1 = new Direccion("Corrientes", 5000)
+    val paradaSubteB2 = new Direccion("Corrientes", 4500)
+    val paradaSubteB3 = new Direccion("Corrientes", 4000)
+    val paradaSubteB4 = new Direccion("Corrientes", 3500)
+    val paradaSubteB5 = new Direccion("Corrientes", 3000)
+
+    val paradaSubteD1 = new Direccion("Corrientes", 5000)
+    val paradaSubteD2 = new Direccion("Santa Fe", 4200)
+    val paradaSubteD3 = new Direccion("Santa Fe", 3400)
+    val paradaSubteD4 = new Direccion("Santa Fe", 2600)
+    val paradaSubteD5 = new Direccion("Santa Fe", 1800)
 
     val paradaTren1 = new Direccion("La via", 0)
     val paradaTren2 = new Direccion("La via", 1000)
@@ -40,16 +47,24 @@ class TestsCostos {
     var paradasDeColec: Array[Direccion] = new Array[Direccion](3)
     var paradasDeSubte: Array[Direccion] = new Array[Direccion](5)
     var paradasDeTren: Array[Direccion] = new Array[Direccion](7)
+    var paradasDeSubteB: Array[Direccion] = new Array[Direccion](5)
+    var paradasDeSubteD: Array[Direccion] = new Array[Direccion](5)
 
     paradasDeColec :+= paradaColec1
     paradasDeColec :+= paradaColec2
     paradasDeColec :+= paradaColec3
 
-    paradasDeSubte :+= paradaSubte1
-    paradasDeSubte :+= paradaSubte2
-    paradasDeSubte :+= paradaSubte3
-    paradasDeSubte :+= paradaSubte4
-    paradasDeSubte :+= paradaSubte5
+    paradasDeSubteB :+= paradaSubteB1
+    paradasDeSubteB :+= paradaSubteB2
+    paradasDeSubteB :+= paradaSubteB3
+    paradasDeSubteB :+= paradaSubteB4
+    paradasDeSubteB :+= paradaSubteB5
+
+    paradasDeSubteD :+= paradaSubteD1
+    paradasDeSubteD :+= paradaSubteD2
+    paradasDeSubteD :+= paradaSubteD3
+    paradasDeSubteD :+= paradaSubteD4
+    paradasDeSubteD :+= paradaSubteD5
 
     paradasDeTren :+= paradaTren1
     paradasDeTren :+= paradaTren2
@@ -64,12 +79,13 @@ class TestsCostos {
     val informador = new StubInformacionTransportes()
     var linea17 = COLECTIVO(paradasDeColec, informador)
     var lineaB = SUBTE(paradasDeSubte, informador)
+    var lineaD = SUBTE(paradasDeSubteD, informador)
     var sarmiento = TREN(paradasDeTren, informador, tablaDePreciosDelTren)
 
   }
 
   @Test
-  def testPrecioBaseDeUnRecorrido = {
+  def testCostoBaseDeUnRecorrido = {
     val seteo = setUp
     var paradas: Array[Direccion] = Array(new Direccion("Corrientes", 41000), new Direccion("Corrientes", 4500))
     var primeraDire: Direccion = new Direccion("avellaneda", 923)
@@ -91,7 +107,7 @@ class TestsCostos {
     assertEquals(0.0, viajeEnColectivo.costoDelViaje(tarjetaDiscapacitado), 0.1)
 
   }
-  
+
   @Test
   def elCostoDeUnRecorridoConTarjetaYendoAlTrabajoDaCorrecto() {
     val seteo = setUp
@@ -120,19 +136,18 @@ class TestsCostos {
 
   }
 
-  
   @Test
   def elCostoDeUnRecorridoConTarjetaTurismoDaCorrecto() {
     val seteo = setUp
     var tarjetaTurista = new TarjetaTurista
 
-    tarjetaTurista.zona :+= seteo.paradaSubte1
-    tarjetaTurista.zona :+= seteo.paradaSubte5
+    tarjetaTurista.zona :+= seteo.paradaSubteB1
+    tarjetaTurista.zona :+= seteo.paradaSubteB5
 
-    val recorridoTurista = new Recorrido(seteo.paradaSubte1, seteo.paradaSubte5, seteo.lineaB)
+    val recorridoTurista = new Recorrido(seteo.paradaSubteB1, seteo.paradaSubteB5, seteo.lineaB)
     val viajeTurista = new ViajeSimple(recorridoTurista)
 
-    assertEquals(4.05 , viajeTurista.costoDelViaje(tarjetaTurista), 0.01)
+    assertEquals(4.05, viajeTurista.costoDelViaje(tarjetaTurista), 0.01)
   }
 
   @Test
@@ -141,7 +156,7 @@ class TestsCostos {
     val recorridoEnColectivo = new Recorrido(seteo.paradaColec1, seteo.paradaColec2, seteo.linea17)
     val viajeEnColectivo = new ViajeSimple(recorridoEnColectivo)
 
-    assertEquals(2.5 , viajeEnColectivo.costoDelViaje(), 0.1)
+    assertEquals(2.5, viajeEnColectivo.costoDelViaje(), 0.1)
 
   }
 
@@ -150,7 +165,7 @@ class TestsCostos {
 
     val seteo = setUp
 
-    val recorridoEnSubte = new Recorrido(seteo.paradaSubte1, seteo.paradaSubte5, seteo.lineaB)
+    val recorridoEnSubte = new Recorrido(seteo.paradaSubteB1, seteo.paradaSubteB5, seteo.lineaB)
     val viajeEnSubte = new ViajeSimple(recorridoEnSubte)
 
     assertEquals(4.5, viajeEnSubte.costoDelViaje(), 0.1)
@@ -164,7 +179,120 @@ class TestsCostos {
     val recorridoEnTren = new Recorrido(seteo.paradaTren4, seteo.paradaTren5, seteo.sarmiento)
     val viajeEnTren = new ViajeSimple(recorridoEnTren)
 
-    assertEquals(2.0 , viajeEnTren.costoDelViaje(), 0.1)
+    assertEquals(2.0, viajeEnTren.costoDelViaje(), 0.1)
 
+  }
+  /**
+   * *****
+   *
+   */
+
+  @Test
+  def siTenemosUnViajeCombinadoConDosSubtesSaleComoSiFueraConUnoSolo {
+    val seteo = setUp
+
+    val recorridoEnSubteB = new Recorrido(seteo.paradaSubteB1, seteo.paradaSubteB5, seteo.lineaB)
+    val recorridoEnSubteD = new Recorrido(seteo.paradaSubteD1, seteo.paradaSubteD3, seteo.lineaD)
+    val viajeEnSubte = new ViajeCompuesto(recorridoEnSubteB, recorridoEnSubteD)
+
+    assertEquals(4.5 , viajeEnSubte.costoDelViaje(), 0.1)
+  }
+
+  @Test
+  def siTenemosUnViajeCombinadoEntreUnTrenYUnSubteDaElPrecioSumadoDeAmbosRecorridos {
+    val seteo = setUp
+
+    val recorridoEnSubteB = new Recorrido(seteo.paradaSubteB1, seteo.paradaSubteB5, seteo.lineaB)
+    val recorridoEnTren = new Recorrido(seteo.paradaTren4, seteo.paradaTren5, seteo.sarmiento)
+    val viajeCombinado = new ViajeCompuesto(recorridoEnSubteB, recorridoEnTren)
+
+    assertEquals(6.5 ,  viajeCombinado.costoDelViaje(), 0.1)
+  }
+
+  @Test
+  def siTenemosUnViajeCombinadoEntreTrenYUnColectivoDaLaSumaDeAmbosRecorridos {
+    val seteo = setUp
+    val recorridoEnColectivo = new Recorrido(seteo.paradaColec1, seteo.paradaColec2, seteo.linea17)
+    val recorridoEnTren = new Recorrido(seteo.paradaTren4, seteo.paradaTren5, seteo.sarmiento)
+    val viajeCombinado = new ViajeCompuesto(recorridoEnColectivo, recorridoEnTren)
+
+    assertEquals(4.5 , viajeCombinado.costoDelViaje(), 0.1)
+
+  }
+
+  @Test
+  def siTenemosUnViajeCombinadoEntreUnTrenYUnColectivoConLaTarjetaDiscapacitadoNosSaleGratisTodoElViaje {
+    val tarjetaDiscapacitado: TarjetaDiscapacitado = new TarjetaDiscapacitado
+
+    val seteo = setUp
+    val recorridoEnColectivo = new Recorrido(seteo.paradaColec1, seteo.paradaColec3, seteo.linea17)
+    val recorridoEnTren = new Recorrido(seteo.paradaTren4, seteo.paradaTren5, seteo.sarmiento)
+
+    val viajeCombinado = new ViajeCompuesto(recorridoEnColectivo, recorridoEnTren)
+
+    assertEquals(0 , viajeCombinado.costoDelViaje(tarjetaDiscapacitado), 0.1)
+
+  }
+
+  @Test
+  def siTenemosUnViajeCombinandoUnColectivoAlTrabajoYUnSubte {
+    val seteo = setUp
+    var tarjetaYendoAlTrabajo: TarjetaTrabajador = new TarjetaTrabajador
+
+    val direccionDeLaBoca = new Direccion("Brandsen", 200)
+    val direccionDelCentro = new Direccion("Riobamba", 700)
+
+    val parada1Del24 = new Direccion("Av. Patricios", 150)
+    val parada3Del24 = new Direccion("Lavalle", 1500)
+
+    var paradasBondi = new Array[Direccion](4)
+    paradasBondi :+= parada1Del24
+    paradasBondi :+= direccionDeLaBoca
+    paradasBondi :+= parada3Del24
+    paradasBondi :+= direccionDelCentro
+
+    tarjetaYendoAlTrabajo.laBoca :+= direccionDeLaBoca
+    tarjetaYendoAlTrabajo.centro :+= direccionDelCentro
+
+    val linea24 = COLECTIVO(paradasBondi, seteo.informador)
+    val recorridoAlTrabajo = new Recorrido(direccionDeLaBoca, direccionDelCentro, linea24)
+    val recorridoEnTren = new Recorrido(seteo.paradaTren4, seteo.paradaTren5, seteo.sarmiento)
+
+    val viajeCombinado = new ViajeCompuesto(recorridoAlTrabajo, recorridoEnTren)
+
+    assertEquals(3.00 , viajeCombinado.costoDelViaje(tarjetaYendoAlTrabajo), 1.00)
+
+  }
+
+  @Test
+  def siTenemosUnViajeCombinadoEntreDosSubtesConLaTarjetaTurismoTeCobraUnSoloViaje {
+    val seteo = setUp
+    var tarjetaTurista = new TarjetaTurista
+
+    tarjetaTurista.zona :+= seteo.paradaSubteB1
+    tarjetaTurista.zona :+= seteo.paradaSubteB5
+
+    val recorridoTurista = new Recorrido(seteo.paradaSubteB1, seteo.paradaSubteB5, seteo.lineaB)
+    val recorridoEnSubteD = new Recorrido(seteo.paradaSubteD1, seteo.paradaSubteD3, seteo.lineaD)
+
+    val viajeCombinado = new ViajeCompuesto(recorridoTurista, recorridoEnSubteD)
+
+    assertEquals(4.05 , viajeCombinado.costoDelViaje(tarjetaTurista), 0.01)
+  }
+
+  @Test
+  def siTenemosUnViajeCombinadoEntreUnSubteYUnTrenConLaTarjetaTurismoTeCobraDescuentoSoloEnElQueAplica {
+    val seteo = setUp
+    var tarjetaTurista = new TarjetaTurista
+
+    tarjetaTurista.zona :+= seteo.paradaSubteB1
+    tarjetaTurista.zona :+= seteo.paradaSubteB5
+
+    val recorridoTurista = new Recorrido(seteo.paradaSubteB1, seteo.paradaSubteB5, seteo.lineaB)
+    val recorridoEnTren = new Recorrido(seteo.paradaTren4, seteo.paradaTren5, seteo.sarmiento)
+
+    val viajeCombinado = new ViajeCompuesto(recorridoTurista, recorridoEnTren)
+
+    assertEquals(6.05 , viajeCombinado.costoDelViaje(tarjetaTurista), 0.01)
   }
 }
