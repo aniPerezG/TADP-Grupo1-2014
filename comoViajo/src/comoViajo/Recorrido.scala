c9f140bb6a9b409067c38f4b0450143137e0f362
Ani <anitaperez2@gmail.com>
2014-07-05 10:40:35 -0300

    elimino el metodo costoDeCombinar de la clase recorrido y la dejo en la clase ViajeCompuesto

--- comoViajo/src/comoViajo/Recorrido.scala
+++ comoViajo/src/comoViajo/Recorrido.scala
@@ -14,18 +14,6 @@ class Recorrido(var paradaDeSubida: Direccion, var paradaDeBajada: Direccion, va
       }
   }
 
-  def costoCombinar(primerRecorrido: Recorrido, segundoRecorrido: Recorrido) =
-    (primerRecorrido.transporte, segundoRecorrido.transporte) match {
-      case (SUBTE(_,_), SUBTE(_,_)) => primerRecorrido.costoBase
-      case _ => primerRecorrido.costoBase + segundoRecorrido.costoBase
-    }
-
-  def costoCombinar(primerRecorrido: Recorrido, segundoRecorrido: Recorrido, tarjeta: Tarjeta) =
-    (primerRecorrido.transporte, segundoRecorrido.transporte) match {
-      case (SUBTE(_,_), SUBTE(_,_)) => tarjeta.precioDeTarjeta(primerRecorrido)
-      case _ => tarjeta.precioDeTarjeta(primerRecorrido) + tarjeta.precioDeTarjeta(segundoRecorrido)
-    } 
-
   def tiempoBase: Double = transporte match {
     case SUBTE(paradas,informador) => SUBTE(paradas,informador).paradasDe(this) * 2
     case TREN(paradas,informador,tabla) => TREN(paradas,informador,tabla).paradasDe(this) * 3
@@ -35,12 +23,5 @@ class Recorrido(var paradaDeSubida: Direccion, var paradaDeBajada: Direccion, va
       
   }
 
-  /*
-    def tiempoDeCombinacionCon(recorrido: Recorrido) : Double = {
-   
-       return transporte.tiempoDeCombinacionEntre(this, recorrido, recorrido.transporte)
-        
-    }
-    */
 
 }
